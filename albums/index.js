2696a4e527b96c0af3006f4a612b31ffad65771f
bilger-progress <bilger.yahov@progress.com>
2019-04-18 21:34:07 +0300

    Design the re-usable "Header" component.

--- albums/index.js
+++ albums/index.js
@@ -1,9 +1,8 @@
-/**
- * @format
- */
-
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import Header from './src/components/header';
+
+const App = () => (<Header headerText={'Albums!'} />);
+
+AppRegistry.registerComponent('albums', () => App);
