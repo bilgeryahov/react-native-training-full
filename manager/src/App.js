40f8e6c414e6e4c6634c22a462e51d553d87440e
bilger-progress <bilger.yahov@progress.com>
2019-05-09 12:39:42 +0300

    Implement end-to-end authentication with Firebase.

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,9 +1,10 @@
 import firebase from 'firebase';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
     componentWillMount() {
@@ -19,13 +20,10 @@ class App extends Component {
     }
 
     render() {
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
         return (
-            <Provider store={createStore(reducers)}>
-                <View>
-                    <Text>
-                        Hello!
-                    </Text>
-                </View>
+            <Provider store={store}>
+                <LoginForm />
             </Provider>
         );
     }
