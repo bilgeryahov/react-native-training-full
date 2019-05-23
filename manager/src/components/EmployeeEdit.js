66cfda074164ee73f2cd234b2783fe728c975fbf
bilger-progress <bilger.yahov@progress.com>
2019-05-23 18:12:49 +0300

    Text employees their shifts.

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { text } from 'react-native-communications';
 import { Card, CardSection, Button } from './common';
 import EmployeeForm from './EmployeeForm';
 import { employeeUpdate, employeeSave } from '../actions';
@@ -15,6 +16,11 @@ class EmployeeEdit extends Component {
         this.props.employeeSave({ name, shift, phone, uid: this.props.employee.uid });
     }
 
+    onTextPress() {
+        const { phone, shift } = this.props;
+        text(phone, `Your upcoming shift is on ${shift}`);
+    }
+
     render() {
         return (
             <Card>
@@ -22,6 +28,11 @@ class EmployeeEdit extends Component {
                 <CardSection>
                     <Button onPress={this.onButtonPress.bind(this)}>
                         Save Changes
+                    </Button>
+                </CardSection>
+                <CardSection>
+                    <Button onPress={this.onTextPress.bind(this)}>
+                        Text Schedule
                     </Button>
                 </CardSection>
             </Card>
